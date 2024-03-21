8516feec4167f722eba11fac5aca3c396f97bdbf
mode30 <emmanuelmodebelu@gmail.com>
2024-03-21 09:32:42 +0100

    model defined

---
+++ signup/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+from .models import Customer
+class CustomerForm(forms.ModelForm):
+     class Meta:Customer
+     fields='__all__'
--- signup/models.py
+++ signup/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    full_name=models.CharField(max_length=50)
+    email=models.CharField(max_length=35,unique=True)
+    
